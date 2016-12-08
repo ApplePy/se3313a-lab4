762279a01d4570d3c9007594f34ceaa7d9020267
Darryl Murray <darrylmurray@me.com>
2016-12-08 07:51:06 +0000

    Added visitor class implementation, setup the flex_waiter (sort of), set up a way to terminate the program, and started hooking up the listening function.

--- server/server/include/server.hpp
+++ server/server/include/server.hpp
@@ -30,6 +30,7 @@
 #include <boost/bimap/unordered_set_of.hpp>
 #include <boost/optional.hpp>
 
+#include "dmurra47_visitor.hpp"
 
 
 namespace dmurra47 
@@ -42,17 +43,26 @@ class server final
 public:
 
     typedef se3313::networking::port_t port_t;
+    typedef se3313::networking::flex_waiter flex;
+    typedef dmurra47::dmurra47_visitor visit;
     
 private:
     
     const port_t _serverPort;
+    
+    flex waiter;		// Holds all the sockets
+    std::thread* listenThread;	// Listens for connections/data and responds.
+    visit visitor;		// Holds class instantiation for visitor.
+    bool killed;		// Holds whether or not server.stop() was called.
     
 public:
 
     inline
     server(const port_t serverPort)
         : _serverPort(serverPort)
-    { }
+    { 
+      killed = false;
+    }
 
     ~server();
 
@@ -71,7 +81,7 @@ public:
 private:
     
     // INSERT YOUR OPERATIONS BELOW
-    
+  void listen();
 
 };
 
--- server/server/src/server.cpp
+++ server/server/src/server.cpp
@@ -23,8 +23,9 @@
 #include <msg/json.hpp>
 
 #include "server.hpp"
+#include "dmurra47_visitor.hpp"
 
-using namespace jsmith2;
+using namespace dmurra47;
 
 
 namespace msg = se3313::msg;
@@ -35,16 +36,36 @@ namespace pt  = boost::property_tree;
 server::~server()
 {
     std::cout << "Stopping the server.";
-    
+    listenThread->join();
 }
 
 void server::start()
 {
     std::cout << "Starting server on port: " << _serverPort << std::endl;
-    
+    waiter = flex(net::socket_server(_serverPort));				// Create server socket and put in flexWaiter
+    visitor = visit(&waiter);								// Setup visitor class
+    listenThread = new std::thread(&server::listen, this);			// Start thread to listen
 }
 
 void server::stop()
 {
-    
+    killed = true;	// Signal end of program
+    waiter.kill();	// Stop the listen loop
 }
+
+void server::listen()
+{
+  try
+  {
+    while(!killed)
+    {
+      int timeout = 1; // One second
+      waiter.wait(&visitor, std::chrono::duration_cast<std::chrono::seconds>(timeout));
+    }
+  }
+  catch (std::runtime_error err)
+  {
+  }
+}
+
+//void server::onSo
